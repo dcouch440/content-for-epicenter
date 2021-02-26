b211b3cf604a76f910c74f6aca8ec2faa29fe4a6
dcouch440 <dcouch440@gmail.com>
2021-02-26 09:44:22 -0800

    Test > get_words > PASS

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,7 +4,7 @@ class Anagram
     @word_two = word_two
   end
   def get_words
-
+    [@word_one, @word_two].map{|w| w.gsub(/\s/, '')}
   end
   def has_vowel?
     [@word_one, @word_two].all?(/[aeiouy]/i)
