8f85d9116eec3fab19aef9741b62096b2fe627ad
dcouch440 <dcouch440@gmail.com>
2021-02-26 09:14:57 -0800

    add progress on has_vowel concept

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,9 @@
 class Anagram
-  def initialize(word)
-    @word = word
+  def initialize(word_one, word_two)
+    @word_one = word_one
+    @word_one = word_two
   end
   def has_vowel?
-    @word
+    [@word_one, @word_two].map {|w| w =~ /[aeiou]/}
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,11 +5,11 @@ require('pry')
 describe("Anagram") do
   describe("#has_vowel") do
     it("returns true if the word contains a vowel") do
-      anagram = Anagram.new('word')
+      anagram = Anagram.new('word', 'word')
       expect(anagram.has_vowel?).to(eq(true))
     end
     it("returns false if the word does not contain a vowel") do
-      anagram = Anagram.new('yurp')
+      anagram = Anagram.new('hbvc', 'hbvc')
       expect(anagram.has_vowel?).to(eq(false))
     end
   end
