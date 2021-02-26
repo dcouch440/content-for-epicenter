91c80ce0cafd9161c83301527348fb34ca56a61b
dcouch440 <dcouch440@gmail.com>
2021-02-26 15:02:22 -0800

    Refector > Move class creation inline

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -11,18 +11,15 @@ describe("Anagram") do
   end
   describe("#has_vowel") do
     it("returns true if the word contains a vowel") do
-      anagram = Anagram.new.set_words('word', 'word')
-      expect(anagram.has_vowel?).to(eq(true))
+      expect(Anagram.new.set_words('word', 'word').has_vowel?).to(eq(true))
     end
     it("returns false if the word does not contain a vowel") do
-      anagram = Anagram.new.set_words('hbvc', 'hbvc')
-      expect(anagram.has_vowel?).to(eq(false))
+      expect(Anagram.new.set_words('hbvc', 'hbvc').has_vowel?).to(eq(false))
     end
   end
   describe("#get_words") do
     it("returns both words with the spaces removes") do
-      anagram = Anagram.new.set_words('test space', 'test space two')
-      expect(anagram.get_words).to(eq(['testspace','testspacetwo']))
+      expect(Anagram.new.set_words('test space', 'test space two').get_words).to(eq(['testspace','testspacetwo']))
     end
   end
   describe("#matching_chars") do
